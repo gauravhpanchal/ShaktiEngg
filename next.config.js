5159a38be5a8e7c0547907e49e3f1526e4d42745
gauravhpanchal <gauravhpanchal2@gmail.com>
2025-09-05 18:42:06 +0530

    Cloudinary Image added

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/photos/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
